e495de6e2242b0d1c6c443e01c32826b0801dfce
rohit1101 <rohit@Rohits-MacBook-Air.local>
2023-03-26 18:06:42 +0530

    desktop banner draft done ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import { useState } from "react";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <div className="h-screen w-full bg-cover items-center p-32 bg-black-50 bg-blend-overlay bg-[url('./assets/profile.webp')] text-white">
+        <nav className="flex justify-between">
+          <p>Rohit S</p>
+          <ul className="flex text-text-gray text-sm gap-16">
+            <li className="cursor-pointer">Articles</li>
+            <li className="cursor-pointer">Contact</li>
+          </ul>
+        </nav>
+        <main className="flex flex-col justify-center h-full max-w-2xl">
+          <h1 className=" leading-tight text-6xl font-semibold">
+            Helping companies build better, scalable software.
+          </h1>
+          <p className="text-text-gray py-12">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor,
+            nesciunt. Architecto sed minima, illum cupiditate optio dolores
+            aspernatur dicta magni autem expedita officia. Earum, minima tempora
+            aliquid ratione repellat ipsam vero! Beatae tempore, facere
+            voluptate expedita magni non unde quia ut asperiores deleniti, et
+            numquam iusto autem, accusamus voluptas voluptatibus!
+          </p>
+        </main>
+      </div>
+      <div className="bg-black text-white">hello</div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#0a0a0b",
+      gray: "#2d3037",
+      yellow: "#fbe84f",
+      white: "#ffffff",
+      "text-gray": "#a0a0a0",
+      "black-50": "rgba(0,0,0,0.6)",
+    },
+    spacing: {
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      5: "5px",
+      6: "6px",
+      7: "7px",
+      8: "8px",
+      9: "9px",
+      10: "10px",
+      12: "12px",
+      16: "16px",
+      32: "32px",
+      48: "48px",
+      450: "450px",
+      550: "550px",
+    },
     extend: {},
   },
   plugins: [],
